afad91182915c70c120e70149da8a2a0772d3f5d
zengzehua <cesc@zzh>
2019-08-29 00:07:58 +0800

    新增:新增声明函数参数相关

--- function/functionDemo.py
+++ function/functionDemo.py
@@ -22,3 +22,29 @@ def count2(list):
 list=[123,1234,12345]
 count2(list)
 print(list)
+
+#函数中的可以传递4种参数
+
+#必需参数:以a,b传递，按顺序传递
+def var(a,b):
+    print(a,b)
+
+#关键字参数
+def var1(a,b):
+    print(a,b)
+
+var1(b=10,a=1)
+
+#默认参数
+def var2(a,b=35):
+    print(a,b)
+
+#不定长参数
+#如果使用*则按元祖传递数据
+def var3(a,*b):
+    print(a,b)
+#如果使用**则按字典传递数据
+def var3(a,**b):
+    print(a,b)
+
+
